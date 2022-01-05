fc35f03bb644d79dd29d33100dae85ba267a53a2
Cyril Halmo <cyril.halmo@gmx.ch>
2022-01-05 01:08:06 +0100

    Added the Baby Calendar Page.
    Added react-calendar and react-calendar-timeline to the project and the Baby Calendar Page.
    Linked the selected day in the calendar to the centrally displayed day in the calendar timeline.
    Some css color shenanigans.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,19 +4,21 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { BabyCalendar } from './components/BabyCalendar';
 
 import './custom.css'
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+                <Route path='/babycalendar' component={BabyCalendar} />
+            </Layout>
+        );
+    }
 }
---
+++ ClientApp/src/components/BabyCalendar.js
@@ -0,0 +1,66 @@
+﻿import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+import Timeline from 'react-calendar-timeline';
+import moment from 'moment';
+
+import 'react-calendar/dist/Calendar.css';
+import 'react-calendar-timeline/lib/Timeline.css';
+import './BabyCalendar.css';
+
+export class BabyCalendar extends Component {
+    static displayName = Calendar.name;
+
+    constructor(props) {
+        super(props);
+        this.timelineChanged = this.timelineChanged.bind(this);
+        this.state = {
+            value: new Date(),
+            items: [
+                {
+                    id: 1,
+                    group: 1,
+                    start_time: moment(),
+                    end_time: moment().add(1, 'hour')
+                }
+            ]
+        };
+    }
+
+    timelineChanged(timeStart, timeEnd, updateCanvas) {
+        updateCanvas(timeStart, timeEnd);
+        this.setState({ value: new Date((timeStart + timeEnd) / 2) });
+    }
+
+    render() {
+
+        let groups = [{ id: 1, title: 'Sleeping' }, { id: 2, title: 'Eating' }, { id: 3, title: 'Playing' }, { id: 4, title: 'Crying' }]
+
+        return (
+            <div>
+                <h1>Baby Calendar</h1>
+                <div>
+                    <Calendar
+                        className="baby-calendar"
+                        value={this.state.value}
+                    />
+                </div>
+                <br />
+                <div>
+                    <Timeline
+                        groups={groups}
+                        items={this.state.items}
+                        defaultTimeStart={moment().add(-12, 'hour')}
+                        defaultTimeEnd={moment().add(12, 'hour')}
+                        canMove={false}
+                        canChangeGroup={false}
+                        canResize={false}
+                        minZoom={86400000}
+                        maxZoom={86400000}
+                        onTimeChange={this.timelineChanged}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+}
