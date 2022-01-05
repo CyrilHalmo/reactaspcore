13723b514d3fc9ccd8e2eef198bf6413a84c1884
Cyril Halmo <cyril.halmo@gmx.ch>
2022-01-05 03:30:44 +0100

    Added Toggle Sleep Button.
    A Button to toggle the Sleep Items on the Timeline.
    Items are being created.

--- ClientApp/src/components/BabyCalendar.js
+++ ClientApp/src/components/BabyCalendar.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from 'react';
 import Calendar from 'react-calendar';
-import Timeline from 'react-calendar-timeline';
+import Timeline, { TimelineMarkers, TodayMarker, CustomMarker } from 'react-calendar-timeline';
 import moment from 'moment';
 
 import 'react-calendar/dist/Calendar.css';
@@ -16,34 +16,63 @@ export class BabyCalendar extends Component {
         this.zoomChanged = this.zoomChanged.bind(this);
         this.calendarChanged = this.calendarChanged.bind(this);
         this.timelineChanged = this.timelineChanged.bind(this);
+        this.toggleSleep = this.toggleSleep.bind(this);
 
         this.state = {
+            sleepItems: 0,
+            isSleep: false,
+            sleepButton: "Start Sleep",
             timeStart: 0,
             timeEnd: 0,
             value: new Date(),
             items: [
-                {
-                    id: 1,
-                    group: 1,
-                    start_time: moment(),
-                    end_time: moment().add(1, 'hour')
-                }
+                //{
+                //    id: 1,
+                //    group: 1,
+                //    start_time: moment(),
+                //    end_time: moment().add(1, 'hour')
+                //}
             ]
         };
     }
 
     zoomChanged(context) {
-        var a = (context.visibleTimeStart + context.visibleTimeEnd) / 2;
-        this.setState({ timeStart: a - 43200000, timeEnd: a + 43200000, value: new Date(a) });
+        this.setState({ timelineCenter: (context.visibleTimeStart + context.visibleTimeEnd) / 2 });
+        this.setState({ timeStart: this.state.timelineCenter - 43200000, timeEnd: this.state.timelineCenter + 43200000, value: new Date(this.state.timelineCenter) });
     }
 
     calendarChanged(value) {
+        this.setState({ timelineCenter: (value.getTime() + value.getTime() + 86400000) / 2 });
         this.setState({ timeStart: value.getTime(), timeEnd: value.getTime() + 86400000, value: new Date(value) });
     }
 
     timelineChanged(timeStart, timeEnd, updateCanvas) {
+        this.setState({ timelineCenter: (timeStart + timeEnd) / 2 });
         this.setState({ timeStart: timeStart, timeEnd: timeEnd, value: new Date((timeStart + timeEnd) / 2) });
         updateCanvas(timeStart, timeEnd);
+    }
+
+    toggleSleep() {
+        if (this.state.isSleep)
+            this.stopSleep();
+        else
+            this.startSleep();
+    }
+
+    stopSleep() {
+        this.setState({ isSleep: false, sleepButton: "Start Sleep" });
+    }
+
+    startSleep() {
+        this.setState({ sleepItems: this.state.sleepItems + 1, isSleep: true, sleepButton: "Stop Sleep" });
+        this.state.items.push(
+            {
+                id: this.state.sleepItems,
+                group: 1,
+                start_time: moment(),
+                end_time: moment()
+            }
+        );
     }
 
     render() {
@@ -76,7 +105,17 @@ export class BabyCalendar extends Component {
                         visibleTimeStart={this.state.timeStart}
                         visibleTimeEnd={this.state.timeEnd}
                         onZoom={this.zoomChanged}
-                    />
+                    >
+                        <TimelineMarkers>
+                            <TodayMarker />
+                            <CustomMarker date={this.state.timelineCenter} />
+                        </TimelineMarkers>
+                    </Timeline>
+                </div>
+                <br />
+                <div>
+                    <button type="button" className="btn btn-primary" onClick={this.toggleSleep}>{this.state.sleepButton}</button>
+                    <p>{this.state.sleepItems}</p>
                 </div>
             </div>
         );
